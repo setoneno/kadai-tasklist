39d7d554c23523fd16ea338eccd3c3983806305f
setoneno <setone0222@gmail.com>
2018-09-06 02:18:38 +0000

    0906]

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
-  #:index, :new, :create,
  
 
   before_action :set_task, only: [:show, :edit, :update, :destroy]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:show]
   
-  def index
-    @users = User.all.page(params[:page])
-  end
-
   def show
     @user = User.find(params[:id])
     @tasks = @user.tasks.order('created_at DESC').page(params[:page])
